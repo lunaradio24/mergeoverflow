88662f3b1b478f6c23068eb6cabf8561c7dd388c
Changjun Yeo <lunaradio24@gmail.com>
2024-07-26 11:25:36 +0900

    Build: add entities into imports in each api modules #1

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Account } from './entities/account.entity';
+import { User } from '../users/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Account, User])],
   controllers: [AuthController],
   providers: [AuthService],
 })
