10a80623c8787614c8d6e553ac41d969ce8852be
songsamuel <songsme2886@gmail.com>
2024-07-29 15:25:50 +0900

    Feat/비밀번호 변(테스트X) #5

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,28 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request, HttpStatus, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Request,
+  HttpStatus,
+  Req,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateDetailUserDto } from './dto/create-detail.dto';
 import { UserToInterestDto } from '../interest/dto/userToInterest.dto';
+import { UpdatePassWordDto } from './dto/updatePassWord.dto';
 
 @UseGuards() // <- Jwt 토큰 필요
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  // 내 정보 조회 , 프로필 조회,
+  // 프로필 조회, 내 정보 조회,
   // 아무 것도 안 쓰는 것은 AccessToken을 통해서 누구인지 자동으로 인지
   // req.user는 Jwt 토큰에서 추출 할 것
   // @Request() req 를 왜 사용하느냐? req.user를 사용하기 위해서 사용한다. @Request() req를 안 쓰면 req.user를 못 사용함.
@@ -36,34 +50,26 @@ export class UsersController {
     };
   }
 
-  // @Post('me/interests')
-  // async createDetailUser(@Body() createDetailUserDto: CreateDetailUserDto) {
-  //   const data = await this.usersService.createDetailUser(createDetailUserDto);
+  // 영진님 쓰세요.
+  @UseGuards()
+  @Patch(':id')
+  async createDetailUser(@Body() createDetailUserDto: CreateDetailUserDto) {
+    const data = await this.usersService.createDetailUser(createDetailUserDto);
 
-  //   return data;
-  // }
+    return data;
+  }
 
-  // // Interest 수정
-  // @Post('my')
-  // async createInterest(@Req() req: any, @Body() interestDto: InterestDto) {
-  //   const data = await this.usersService.createUserInterest(req.user, interestDto);
-
-  //   return {
-  //     statusCode: HttpStatus.OK,
-  //     message: '관심사 생성에 성공했습니다.',
-  //     data,
-  //   };
-  // }
+  //비밀번호 수정
+  @UseGuards()
+  @Patch(':id')
+  async updatePassWord(@Param('id', ParseIntPipe) id: number, @Body() updatePassWordDto: UpdatePassWordDto) {
+    return this.usersService.updatePassWord(id, updatePassWordDto);
+  }
 
   // // Interest 조회
   // @Get()
   // findOne(@Param('id') id: string) {
   //   return this.usersService.findOne(+id);
-  // }
-
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-  //   return this.usersService.update(+id, updateUserDto);
   // }
 
   // @Delete(':id')
