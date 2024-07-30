491fb17753aabc79d7f3ccfb2892efa340eb7c59
JoYoungJinNode5 <jyj950406@naver.com>
2024-07-30 19:50:53 +0900

    Fix: sign-in.dto를 사용할 수 있게 수정 #4

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -2,6 +2,7 @@ import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from '../auth.service';
+import { SignInDto } from '../dto/sign-in.dto';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
@@ -10,7 +11,11 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(phoneNum: string, password: string): Promise<any> {
-    const user = await this.authService.validateUser(phoneNum, password);
+    const signInDto = new SignInDto();
+    signInDto.phoneNum = phoneNum;
+    signInDto.password = password;
+
+    const user = await this.authService.validateUser(signInDto);
     if (!user) {
       throw new UnauthorizedException('잘못된 전화번호 또는 비밀번호입니다.');
     }
