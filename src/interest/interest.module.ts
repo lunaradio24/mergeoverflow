dd796d02ddf4f51ba216ddc5165b24b5554a5c71
songsamuel <songsme2886@gmail.com>
2024-07-31 16:52:25 +0900

    Perf/ interest,tech accesstoken 코드 추가 #15, #16

--- src/interest/interest.module.ts
+++ src/interest/interest.module.ts
@@ -4,9 +4,10 @@ import { InterestController } from './interest.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { Interest } from '../users/entities/interest.entity';
+import { Account } from 'src/auth/entities/account.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Interest])],
+  imports: [TypeOrmModule.forFeature([User, Interest, Account])],
   controllers: [InterestController],
   providers: [InterestService],
 })
---
+++ src/s3/s3.service.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { S3Client } from '@aws-sdk/client-s3';
+
+@Injectable()
+export class S3Service {}
