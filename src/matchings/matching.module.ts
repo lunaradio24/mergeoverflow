7dfe7b406fb8b3dc8d16f3879f9d7f3717a1cf1c
yooseungyeop <yooseungyub@naver.com>
2024-08-20 05:54:23 +0900

    Feat : matching logic add techs

--- src/matchings/matching.module.ts
+++ src/matchings/matching.module.ts
@@ -13,10 +13,14 @@ import { Location } from '../locations/entities/location.entity';
 import { LocationService } from '../locations/location.service';
 import { LocationController } from '../locations/location.controller';
 import { UserModule } from 'src/users/user.module';
+import { Tech } from 'src/techs/entities/tech.entity';
+import { UserToTech } from 'src/users/entities/user-to-tech.entity';
+import { TechModule } from 'src/techs/tech.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, Heart, Matching, Preferences, Location]),
+    TypeOrmModule.forFeature([User, Heart, Matching, Preferences, Location, Tech, UserToTech]),
+    TechModule,
     ChatRoomModule,
     NotificationModule,
     UserModule,
--- src/preferences/entities/preferences.entity.ts
+++ src/preferences/entities/preferences.entity.ts
@@ -24,6 +24,9 @@ export class Preferences {
   @Column({ type: 'int', nullable: false })
   userId: number;
 
+  @Column({ type: 'json', nullable: true })
+  techs: string[];
+
   @Column({ type: 'enum', enum: PreferredCodingLevel, nullable: true })
   codingLevel: PreferredCodingLevel;
 
