c3ad050ade041df0b7e33d5a71159a82f9b45255
Changjun Yeo <lunaradio24@gmail.com>
2024-08-20 19:28:27 +0900

    Fix: 로그인, 회원가입 dto 데코레이터 수정 #4

--- src/auth/dto/local-sign-in.dto.ts
+++ src/auth/dto/local-sign-in.dto.ts
@@ -1,10 +1,13 @@
-import { IsString, Matches } from 'class-validator';
+import { IsNotEmpty, IsString, Matches } from 'class-validator';
+import { AUTH_MESSAGES } from '../constants/auth.message.constant';
 
 export class LocalSignInDto {
+  @IsNotEmpty({ message: AUTH_MESSAGES.COMMON.PHONE_NUM.REQUIRED })
   @IsString()
-  @Matches(/^010-\d{4}-\d{4}$/, { message: '전화번호 형식이 올바르지 않습니다. (예: 010-1234-5678)' })
+  @Matches(/^010-\d{4}-\d{4}$/, { message: AUTH_MESSAGES.COMMON.PHONE_NUM.INVALID_FORMAT })
   phoneNum: string;
 
+  @IsNotEmpty({ message: AUTH_MESSAGES.COMMON.PASSWORD.REQUIRED })
   @IsString()
   password: string;
 }
